57ff048c3580db25f62708c2cf6bc5d662a66b4e
Max Witt <kirayatail@gmail.com>
2021-08-18 11:46:00 +0200

    Pocket version of installer

--- installer.lua
+++ installer.lua
@@ -1,4 +1,4 @@
---3
+--4
 local url = 'https://raw.githubusercontent.com/kirayatail/computercraft/master/'
 local fileList = {}
 local offset = 0
@@ -47,6 +47,59 @@ function update()
 end
 
 function display()
+    if pocket then 
+        updateOffset(8)
+        displayPocket()
+    else
+        updateOffset(15)
+        displayStandard()
+    end
+end
+
+function updateOffset(lines)
+    if cursor > offset + lines then
+        offset = cursor - lines
+    end
+    if cursor <= offset then
+        offset = cursor - 1
+    end
+end
+
+function displayPocket()
+    term.clear()
+    term.setCursorPos(3,1)
+    term.write('Pocket Installer')
+    term.setCursorPos(2,2)
+    term.setBackgroundColor(colors.white)
+    term.setTextColor(colors.black)
+    term.write('                        ')
+    term.setBackgroundColor(colors.black)
+    term.setTextColor(colors.white)
+    for line = 1,8 do
+        if line == cursor - offset then
+            term.setCursorPos(1, line*2 + 1)
+            term.write('>')
+        end
+        if (line + offset <= table.getn(fileList)) then
+            local file = fileList[line + offset]
+            term.setCursorPos(3, line*2 + 1)
+            term.write(file.name)
+            term.setCursorPos(3, line*2 + 2)
+            term.write(string.format("Version: %d Local: %d", file.version, file.localVersion or ""))
+        end
+    end
+    term.setCursorPos(1, 20)
+    term.blit('i', 'f', '0')
+    term.write('nstall ')
+    term.blit('r', 'f', '0')
+    term.write('emove ')
+    term.blit('u', 'f', '0')
+    term.write('pdate ')
+    term.blit('q', 'f', '0')
+    term.write('uit')
+end
+
+function displayStandard()
     term.clear()
     term.setCursorPos(3,1)
     term.write('Program Installer')
@@ -61,7 +114,7 @@ function display()
             term.setCursorPos(1, line + 2)
             term.write('>')
         end
-        if (line <= table.getn(fileList) - offset) then
+        if (line + offset <= table.getn(fileList)) then
             local file = fileList[line + offset]
             term.setCursorPos(3, line + 2)
             term.write(file.name)
