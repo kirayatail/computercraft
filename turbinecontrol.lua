1e5b6367e002f94cba83d0249883856479434036
Max Witt <kirayatail@gmail.com>
2022-09-05 00:25:13 +0200

    Turbinecontrol should not crash when rendering non-existing WS ID

--- turbinecontrol.lua
+++ turbinecontrol.lua
@@ -1,4 +1,4 @@
---14
+--15
 local t = nil
 local state = {}
 local socket = nil
@@ -324,7 +324,7 @@ function displayInfo()
     term.setCursorPos(26, 7)
     if socket then
         if socket.isConnected() then
-            term.write('Websocket ID: '..socket.id())
+            term.write('Websocket ID: '..(socket.id() or '')
         else
             term.write('Websocket available')
             term.setCursorPos(26, 9)
