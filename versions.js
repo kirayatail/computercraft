7153f45e67cd3f649e6edbee93b774ac72008a62
Max Witt <kirayatail@gmail.com>
2023-12-02 20:32:40 +0100

    ignore leading space for version detection

--- versions.js
+++ versions.js
@@ -1,49 +1,65 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
+const fs = require("fs");
+const { execSync } = require("child_process");
 
-const stdout = execSync('git diff --cached --name-status', {encoding: 'utf8'})
-
-const diff = stdout.split(/[\r\n]+/)
-.filter(n => (/\.lua$/).test(n))
-.map(row => {
-  return {
-    method: row.split(/[\t\s]+/)[0],
-    filename: row.split(/[\t\s]+/)[1]
-  }
+const stdout = execSync("git diff --cached --name-status", {
+  encoding: "utf8",
 });
 
-const oldList = JSON.parse(fs.readFileSync('./list.json', 'utf8'))
+const diff = stdout
+  .split(/[\r\n]+/)
+  .filter((n) => /\.lua$/.test(n))
+  .map((row) => {
+    return {
+      method: row.split(/[\t\s]+/)[0],
+      filename: row.split(/[\t\s]+/)[1],
+    };
+  });
 
-const newFiles = diff.filter(f => f.method === 'M' || f.method === 'A')
-.map(({filename}) => {
-  version = parseInt(fs.readFileSync(filename, 'utf8').split(/--(\d+)/)[1])
-  return {
-    name: filename,
-    version
-  }
-});
+const oldList = JSON.parse(fs.readFileSync("./list.json", "utf8"));
 
-const removedFiles = diff.filter(f => f.method === 'D').map(({filename}) => filename);
+const newFiles = diff
+  .filter((f) => f.method === "M" || f.method === "A")
+  .map(({ filename }) => {
+    version = parseInt(
+      fs.readFileSync(filename, "utf8").split(/--\s?(\d+)/)[1]
+    );
+    return {
+      name: filename,
+      version,
+    };
+  });
 
-const allCorrect = newFiles.every(newFile => {
-  const oldFile = oldList.find(f => f.name === newFile.name);
+const removedFiles = diff
+  .filter((f) => f.method === "D")
+  .map(({ filename }) => filename);
+
+const allCorrect = newFiles.every((newFile) => {
+  const oldFile = oldList.find((f) => f.name === newFile.name);
   const correct = !(oldFile && oldFile.version >= newFile.version);
   if (!correct) {
     console.error(`Version in ${newFile.name} must be updated`);
   }
-  return correct
+  return correct;
 });
 
 if (allCorrect) {
-  console.log(`Updating ${diff.length} file record${diff.length === 1 ? '' : 's'}`);
-  fs.writeFileSync('./list.json', JSON.stringify(
-    oldList
-    .filter(({name}) => !removedFiles.includes(name))
-    .reduce((newList, oldFile) => {
-      return (newList.find(f => f.name === oldFile.name)) ? newList : newList.concat([oldFile]);
-    }, newFiles).sort((a,b) => (a.name > b.name) ? 1 : (a.name < b.name) ? -1 : 0)
-  ));
-  execSync('git add list.json ');
+  console.log(
+    `Updating ${diff.length} file record${diff.length === 1 ? "" : "s"}`
+  );
+  fs.writeFileSync(
+    "./list.json",
+    JSON.stringify(
+      oldList
+        .filter(({ name }) => !removedFiles.includes(name))
+        .reduce((newList, oldFile) => {
+          return newList.find((f) => f.name === oldFile.name)
+            ? newList
+            : newList.concat([oldFile]);
+        }, newFiles)
+        .sort((a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0))
+    )
+  );
+  execSync("git add list.json ");
   process.exit(0);
 } else {
   process.exit(1);
