393b6ab7d379eec9b3e65b425000069d17b2972c
Max Witt <kirayatail@gmail.com>
2025-04-09 00:31:24 +0200

    File formatting, add join to table library

--- lib/table.lua
+++ lib/table.lua
@@ -1,40 +1,57 @@
---3
+-- 4
 local function filter(tbl, f)
-  if tbl == nil then return nil end
-  if f == nil then return {} end
+  if tbl == nil then
+    return nil
+  end
+  if f == nil then
+    return {}
+  end
   local t = {}
-  for k,v in pairs(tbl) do 
-    if f(v,k,tbl) then t[#t+1] = v end
+  for k, v in pairs(tbl) do
+    if f(v, k, tbl) then
+      t[#t + 1] = v
+    end
   end
   return t
 end
 local function find(tbl, f)
-  if tbl == nil then return nil end
-  if f == nil then return nil end
-  for k,v in pairs(tbl) do
-    if f(v,k,tbl) then return v, k end
+  if tbl == nil or f == nil then
+    return nil
+  end
+  for k, v in pairs(tbl) do
+    if f(v, k, tbl) then
+      return v, k
+    end
   end
   return nil
 end
 local function indexOf(tbl, val)
   local index = {}
-  for k,v in pairs(tbl) do
-      index[v] = k
+  for k, v in pairs(tbl) do
+    index[v] = k
   end
   return index[val] or -1
 end
 local function map(tbl, f)
-  if tbl == nil then return nil end
-  if f == nil then return tbl end
+  if tbl == nil then
+    return nil
+  end
+  if f == nil then
+    return tbl
+  end
   local t = {}
-  for k,v in pairs(tbl) do
-    t[k] = f(v,k,tbl)
+  for k, v in pairs(tbl) do
+    t[k] = f(v, k, tbl)
   end
   return t
 end
 local function reduce(tbl, f, initial)
-  if tbl == nil then return nil end
-  if f == nil then return tbl end
+  if tbl == nil then
+    return nil
+  end
+  if f == nil then
+    return tbl
+  end
   local result
   local startIndex = 1
   if initial then
@@ -43,18 +60,20 @@ local function reduce(tbl, f, initial)
     result = tbl[1]
     startIndex = 2
   end
-  for i = startIndex,#tbl do
+  for i = startIndex, #tbl do
     result = f(result, tbl[i], i, tbl)
   end
   return result
 end
 local function push(tbl, item)
-  if tbl == nil then return nil end
+  if tbl == nil then
+    return nil
+  end
   tbl[#tbl + 1] = item
   return tbl
 end
 local function some(tbl, predicate)
-  for k,v in pairs(tbl) do
+  for k, v in pairs(tbl) do
     if predicate(v, k) then
       return true
     end
@@ -62,17 +81,28 @@ local function some(tbl, predicate)
   return false;
 end
 local function every(tbl, predicate)
-  for k,v in pairs(tbl) do
-    if predicate(v,k) == false then
+  for k, v in pairs(tbl) do
+    if predicate(v, k) == false then
       return false
     end
   end
   return true
 end
+local function join(tbl, delimiter)
+  local result = ""
+  for i = 1, #tbl do
+    result = result .. tbl[i]
+    if i < #tbl then
+      result = result .. delimiter
+    end
+  end
+  return result
+end
 return {
   filter = filter,
   find = find,
   indexOf = indexOf,
+  join = join,
   map = map,
   reduce = reduce,
   push = push,
