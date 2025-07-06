ac115a93ca7a92756d778b675397cfbea60cd361
Max Witt <kirayatail@gmail.com>
2025-07-06 10:32:35 +0200

    RightAlign text for dynamic smeltery values

--- lib/term.lua
+++ lib/term.lua
@@ -1,4 +1,4 @@
---1
+-- 2
 local function out(value, row, col)
   if row ~= nil and col ~= nil then
     term.setCursorPos(col, row)
@@ -9,10 +9,16 @@ local function initial(value, bg, fg, row, col)
   if row ~= nil and col ~= nil then
     term.setCursorPos(col, row)
   end
-  term.blit(string.sub(value, 1,1), bg, fg)
-  term.write(string.sub(value, 2,-1))
+  term.blit(string.sub(value, 1, 1), bg, fg)
+  term.write(string.sub(value, 2, -1))
+end
+local function rightAlign(value, row, col)
+  local length = #(tostring(val))
+  term.setCursorPos(col + 1 - #(tostring(value)), row)
+  term.write(value)
 end
 return {
   initial = initial,
-  out = out
+  out = out,
+  rightAlign = rightAlign
 }
