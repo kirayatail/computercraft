77baf7e6d3de0c4018cee022e681599c43a4d6eb
Max Witt <kirayatail@gmail.com>
2026-03-03 00:15:19 +0100

    batmon three loop configuration

--- batmon.lua
+++ batmon.lua
@@ -1,9 +1,12 @@
--- 8
+-- 9
 local socket = nil
 local Table = nil
 local conf = {}
 local total = 0
+local stored = 0
 local bats = {peripheral.find('thermalexpansion:storage_cell')}
+local fullCells = 0
+local partialBats = {}
 local previous = 0
 
 function init()
@@ -29,26 +32,55 @@ function init()
   end
 end
 
-local function sum(tbl)
-  Table.reduce(tbl, function(a, b)
-    return a + b
-  end)
-end
-
 function start()
   if socket then
     socket.connect('Big battery', true)
+  end
+  total = #bats * 50000
+end
+
+function coarseMonitor()
+  while true do
+    local full = 0
+    for chunk = 0, (#bats / 4) do
+      for i = 1, 4 do
+        local index = chunk * 4 + i
+        if bats[index] then
+          local stored = bats[index].getRFStored() or 0
+          if stored > 49975000 then
+            full = full + 1
+          elseif stored > 25000 then
+            partialBats[index] = bats[index]
+          end
+        end
+      end
+      if (full > fullCells) then
+        fullCells = full
+      end
+      sleep(0.5)
+    end
+    fullCells = full
+    sleep(5)
+  end
+end
+
+function fineMonitor()
+  while true do
+    local partialStored = 0
+    for key, bat in pairs(partialBats) do
+      local current = bat.getRFStored() or 0
+      partialStored = partialStored + current
+      if current < 25000 or current > 49975000 then
+        partialBats[key] = nil
+      end
+    end
+    stored = fullCells * 50000 + partialStored / 1000
+    sleep(1)
   end
 end
 
 function monitor()
   while true do
-    local stored = sum(Table.map(bats, function(b)
-      return b.getRFStored()
-    end)) / 1000
-    local total = sum(Table.map(bats, function(b)
-      return b.getRFCapacity()
-    end)) / 1000
     if socket then
       socket.info({{
         key = 'level',
@@ -70,12 +102,12 @@ function monitor()
       }})
     end
     previous = stored
-    sleep(1)
+    sleep(5)
   end
 end
 
 init()
-local loops = {start, monitor}
+local loops = {start, monitor, coarseMonitor, fineMonitor}
 if socket then
   table.insert(loops, socket.runtime)
 end
